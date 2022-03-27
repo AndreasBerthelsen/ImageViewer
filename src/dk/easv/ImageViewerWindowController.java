e8bfe4d8b68f4f40bee6f70b6c4a21e1993ca9a2
AndreasBerthelsen <andr59j9@easv365.dk>
2022-03-27 16:12:34 +0200

    Slideshow

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -3,18 +3,25 @@ package dk.easv;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ImageViewerWindowController
-{
+public class ImageViewerWindowController {
     private final List<Image> images = new ArrayList<>();
+    @FXML
+    private Button startStopBtn;
+    @FXML
+    private TextField txtInput;
     private int currentImageIndex = 0;
 
     @FXML
@@ -23,17 +30,17 @@ public class ImageViewerWindowController
     @FXML
     private ImageView imageView;
 
+    private boolean isSliding = false;
+
     @FXML
-    private void handleBtnLoadAction()
-    {
+    private void handleBtnLoadAction() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
         fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
                 "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));
         List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
 
-        if (!files.isEmpty())
-        {
+        if (!files.isEmpty()) {
             files.forEach((File f) ->
             {
                 images.add(new Image(f.toURI().toString()));
@@ -43,10 +50,8 @@ public class ImageViewerWindowController
     }
 
     @FXML
-    private void handleBtnPreviousAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnPreviousAction() {
+        if (!images.isEmpty()) {
             currentImageIndex =
                     (currentImageIndex - 1 + images.size()) % images.size();
             displayImage();
@@ -54,20 +59,40 @@ public class ImageViewerWindowController
     }
 
     @FXML
-    private void handleBtnNextAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnNextAction() {
+        if (!images.isEmpty()) {
             currentImageIndex = (currentImageIndex + 1) % images.size();
             displayImage();
         }
     }
 
-    private void displayImage()
-    {
-        if (!images.isEmpty())
-        {
+    private void displayImage() {
+        if (!images.isEmpty()) {
             imageView.setImage(images.get(currentImageIndex));
         }
     }
+
+    public void handleBtnStart(ActionEvent actionEvent)  {
+
+        if (!isSliding){
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+            Runnable slideshow = this::handleBtnNextAction;
+            int delay = Integer.parseInt(txtInput.getText());
+            Future<?> future = executor.scheduleAtFixedRate(slideshow, delay, delay, TimeUnit.SECONDS);
+            future.cancel(true);
+            isSliding = true;
+            startStopBtn.setText("Stop");
+
+        }
+        if (!executor.isShutdown() && isSliding) {
+            executor.shutdown();
+            isSliding = false;
+            startStopBtn.setText("Start");
+        }
+
+
+
+    }
+
+
 }
